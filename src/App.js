b884b85edbf3be5dd2c7725e2c20adfc04c9516e
thals1214 <thaxls@ewhain.net>
2021-09-30 21:57:07 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './Header'
+import Contents from './Contents'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Contents />
+    </div>
+  )
+}
+
+export default App
